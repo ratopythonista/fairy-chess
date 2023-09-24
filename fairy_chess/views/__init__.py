d7e853a60780f346664d604a25140784e9a650fd
Rodrigo Guimarães <rodrigo.guimaraes@biso.digital>
2023-09-24 18:13:42 -0300

    chore: login page

--- fairy_chess/views/__init__.py
+++ fairy_chess/views/__init__.py
@@ -1,9 +1,21 @@
-from flet import Page
+from flet import Page, RouteChangeEvent
 
+from fairy_chess.views.login import LoginPage
 from fairy_chess.views.registration import RegistrationPage
  
+page_mapping = {
+    "/login": LoginPage,
+    "/registration": RegistrationPage
+}
+
 def web_view(page: Page):
     page.title = "Fairy Chess - TFT Tournment - Registration"
     page.padding = 0
-    RegistrationPage(page)
+    LoginPage(page)
+    def route_change(e: RouteChangeEvent):
+        page.clean()
+        page_mapping[e.route](page)
+
+    page.on_route_change = route_change
     page.update()
+       
